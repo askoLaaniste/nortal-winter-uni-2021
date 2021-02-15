bb4f2830cd7ad1d81d0ff8a7c624037603d05c9b
Alvar Sokk <alvar195@hotmail.com>
2021-02-15 15:36:47 +0200

    Ex5 lombok annotations exercise comment

--- src/main/java/demo/exercises/Ex5.java
+++ src/main/java/demo/exercises/Ex5.java
@@ -5,16 +5,16 @@ import lombok.*;
 public class Ex5 {
 
 	public static void main(String[] args) {
-		User user = new User();
-		user.setId(1L);
-		user.setUsername("admin");
-		User user2 = new User(2L, "user");
-		System.out.println(user.getId() + " " + user.getUsername());
-		System.out.println(user2.toString());
+//		User user = new User();
+//		user.setId(1L);
+//		user.setUsername("admin");
+//		User user2 = new User(2L, "user");
+//		System.out.println(user.getId() + " " + user.getUsername());
+//		System.out.println(user2.toString());
 	}
 }
 
-// add Lombok annotations
+// TODO add Lombok annotations
 class User {
 
 	private Long id;
